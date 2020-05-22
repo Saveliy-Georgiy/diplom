9d3ef942fec9021634f758dab37eb356d6491a58
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-05-22 15:54:00 +0300

    Updates

--- precache-manifest.6c126ec8c860b23bd5f23cddc7063c23.js
+++ precache-manifest.6c126ec8c860b23bd5f23cddc7063c23.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "22e803f4863ead24c3b74e5ac063afa9",
+    "revision": "e9b05523ccc225aeb2fe23cf4d6918d2",
     "url": "/diplom/index.html"
   },
   {
-    "revision": "7fe2a9813c6d561dbd03",
+    "revision": "424a7abdc3eebb40e314",
     "url": "/diplom/static/css/main.5bd26dd3.chunk.css"
   },
   {
-    "revision": "469b151ba4b21ea45196",
-    "url": "/diplom/static/js/2.c0663acb.chunk.js"
+    "revision": "ecd2b24a08d49840e36b",
+    "url": "/diplom/static/js/2.d4f63d86.chunk.js"
   },
   {
     "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
-    "url": "/diplom/static/js/2.c0663acb.chunk.js.LICENSE.txt"
+    "url": "/diplom/static/js/2.d4f63d86.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7fe2a9813c6d561dbd03",
-    "url": "/diplom/static/js/main.0f4a3ed7.chunk.js"
+    "revision": "424a7abdc3eebb40e314",
+    "url": "/diplom/static/js/main.6316fbc1.chunk.js"
   },
   {
     "revision": "19a1e56a74204b504acf",
