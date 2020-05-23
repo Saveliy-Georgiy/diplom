f64f33569a6f7cb0d6c76980faabd122b17ef51e
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-05-23 12:41:32 +0300

    Fix links to anchors and some styles

--- src/components/FullPortfolio/Pages/Header/Menu.jsx
+++ src/components/FullPortfolio/Pages/Header/Menu.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import styles from './Menu.module.css';
 import {Link} from "react-router-dom";
+import {HashLink} from 'react-router-hash-link';
 
 function Menu() {
     return (
         <div className={styles.menu}>
             <Link to={'/'} className={styles.link}>Home</Link>
-            <a href="#skills" className={styles.link}>
-                Skills
-            </a>
-            <a href="#projects" className={styles.link}>
-                Projects
-            </a>
-            <a href="#hire" className={styles.link}>
-                Hire
-            </a>
+            <HashLink to={'#skills'} className={styles.link}>Skills</HashLink>
+            <HashLink to={'#projects'} className={styles.link}>Projects</HashLink>
+            <HashLink to={'#hire'} className={styles.link}>Hire</HashLink>
         </div>
     );
 }
